3ab80d25003e4ecce7a620e020001ec3d6d5d977
Mikołaj Karkowski <mikikarkowski@gmail.com>
2026-06-28 01:18:24 +0200

    Add IngredientsSeeder to dependency injection and update project file for embedded resources

--- src/backend/Kotlet.Infrastructure/DependencyInjection.cs
+++ src/backend/Kotlet.Infrastructure/DependencyInjection.cs
@@ -34,6 +34,7 @@ public static class DependencyInjection
         AddDatabase(services, configuration);
         services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
         services.AddScoped<DatabaseSeeder>();
+        services.AddScoped<IngredientsSeeder>();
         services.AddScoped<IngredientCsvSeeder>();
         return services;
     }
